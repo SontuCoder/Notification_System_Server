1215b9ef1286b178fa9cc5623ec155ff5379628f
Subhadip Maity <167627379+SontuCoder@users.noreply.github.com>
2026-06-08 11:59:26 +0530

    NotificationTemplate model Created

--- app/models/template.py
+++ app/models/template.py
@@ -1,1 +1,65 @@
 
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID, JSONB
+from sqlalchemy import Boolean, String, DateTime, Enum as SQLEnum,Text
+from uuid import UUID, uuid4
+from datetime import datetime
+from typing import Any
+
+from app.core.database import Base
+from app.schemas.notification import Notification_Channel
+
+
+class NotificationTemplate(Base):
+    __tablename__ = "notification_templates"
+
+    id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False
+    )
+
+    channel: Mapped[Notification_Channel] = mapped_column(
+        SQLEnum(Notification_Channel),
+        nullable=False
+    )
+
+    title: Mapped[str|None] = mapped_column(
+        Text,
+        nullable=True
+    )
+
+    body: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+
+    variables: Mapped[dict[str, Any] | None] = mapped_column(
+        JSONB,
+        nullable=True
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False,
+        default=True
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
