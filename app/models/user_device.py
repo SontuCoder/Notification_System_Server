dc54bf9be683df8c6cc4516157ea2528af98498a
Subhadip Maity <167627379+SontuCoder@users.noreply.github.com>
2026-06-08 11:18:13 +0530

    UserDevice model Create

--- app/models/user_device.py
+++ app/models/user_device.py
@@ -1,25 +1,63 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy import String, Boolean, DateTime, Index
 from uuid import UUID, uuid4
+from datetime import datetime
 
 from app.core.database import Base
+from app.schemas.notification import DevicePlatform
+
 
 class UserDevice(Base):
+
     __tablename__ = "user_devices"
 
-    id : Mapped[UUID] = mapped_column(
+    __table_args__ = (
+        Index("idx_user_devices_user_id", "user_id")
+    )
+
+    id: Mapped[UUID] = mapped_column(
         PG_UUID(as_uuid=True),
         primary_key=True,
         default=uuid4
     )
 
     user_id: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True),
-
+        PG_UUID(as_uuid=True),
+        nullable=False
     )
-    device_token
-    platform
-    notifications_enabled
-    is_active
-    created_at
-    updated_at
+
+    device_token: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+
+    platform: Mapped[DevicePlatform] = mapped_column(
+        String(20),
+        nullable=False
+    )
+
+    notifications_enabled: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+
