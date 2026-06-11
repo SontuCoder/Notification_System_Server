334de84556f7d92f958869cb07ee5b37db8e0ddf
Subhadip Maity <167627379+SontuCoder@users.noreply.github.com>
2026-06-11 21:15:14 +0530

    Add methods to update preferences by user ID

--- app/repositories/preference_repository.py
+++ app/repositories/preference_repository.py
@@ -154,6 +154,97 @@ class UserNotificationPreferenceRepo:
             db.rollback()
             logger.exception(f"Failed to update user notification preference due {str(ex)}")
             raise
+    
+    def update_push_enabled_by_user_id( self, db: Session, status: bool, user_id: UUID) -> UserNotificationPreference | None:
+    
+        try:
+            preference = self.get_by_user_id(db, user_id)
+            if not preference:
+                return None
+        
+            if preference.push_enabled == status:
+                return preference
+            preference.push_enabled = status
+            self._save(db, preference)
+            logger.info(f"User push notification preference updated successfully for user {preference.user_id}")
+            return preference
+        except SQLAlchemyError as ex:
+            db.rollback()
+            logger.exception(f"Failed to update user push notification preference due {str(ex)}")
+            raise
+    
+    def update_email_enabled_by_user_id(self, db: Session, status: bool, user_id: UUID)-> UserNotificationPreference | None:
+        try:
+            preference = self.get_by_user_id(db, user_id)
+            if not preference:
+                return None
+            
+            if preference.email_enabled == status:
+                return preference
+            preference.email_enabled = status
+            self._save(db, preference)
+            logger.info(f"User email notification preference updated successfully for user {preference.user_id}")
+            return preference
+        except SQLAlchemyError as ex:
+            db.rollback()
+            logger.exception(f"Failed to update user email notification preference due {str(ex)}")
+            raise
+
+    def update_sms_enabled_by_user_id(self, db: Session, status: bool, user_id: UUID)-> UserNotificationPreference | None:
+        try:
+            preference = self.get_by_user_id(db, user_id)
+            if not preference:
+                return None
+            
+            if preference.sms_enabled == status:
+                return preference
+            preference.sms_enabled = status
+            self._save(db, preference)
+            logger.info(f"User sms notification preference updated successfully for user {preference.user_id}")
+            return preference
+        except SQLAlchemyError as ex:
+            db.rollback()
+            logger.exception(f"Failed to update user sms notification preference due {str(ex)}")
+            raise
+
+    def update_promotion_enabled_by_user_id(self, db: Session, status: bool, user_id: UUID)-> UserNotificationPreference | None:
+        try:
+            preference = self.get_by_user_id(db, user_id)
+            if not preference:
+                return None
+            
+            if preference.promotion_enabled == status:
+                return preference
+            preference.promotion_enabled = status
+            self._save(db, preference)
+            logger.info(f"User promotional notification preference updated successfully for user {preference.user_id}")
+            return preference
+        except SQLAlchemyError as ex:
+            db.rollback()
+            logger.exception(f"Failed to update user promotion notification preference due {str(ex)}")
+            raise
+
+    def update_all_preferences_by_user_id(self, db: Session, email_status: bool, push_status: bool, sms_status: bool, promotion_status: bool, user_id: UUID)-> UserNotificationPreference | None:
+        try:
+            preference = self.get_by_user_id(db, user_id)
+            if not preference:
+                return None
+            
+            if preference.email_enabled == email_status and preference.push_enabled == push_status and preference.sms_enabled == sms_status and preference.promotion_enabled == promotion_status:
+                return preference
+            
+            preference.email_enabled = email_status
+            preference.push_enabled = push_status
+            preference.sms_enabled = sms_status
+            preference.promotion_enabled = promotion_status
+            self._save(db, preference)
+            logger.info(f"User all notification preference updated successfully for user {preference.user_id}")
+            return preference
+        except SQLAlchemyError as ex:
+            db.rollback()
+            logger.exception(f"Failed to update user notification preference due {str(ex)}")
+            raise
+
 
 
     # ============= Delete =============
